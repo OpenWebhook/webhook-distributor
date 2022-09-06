0f204d58c497fc30fa801010abfb163c06414c19
Samox <sammy.teillet@gmail.com>
2022-09-06 14:28:01 +0200

    test(): Refactor test using cold helper

--- src/app.saga.spec.ts
+++ src/app.saga.spec.ts
@@ -11,6 +11,20 @@ const target2 = 'http://target2.com';
 const idempotentKey = 'idempotentKey';
 const idempotentKey2 = 'idempotentKey2';
 
+const expectedObservable = {
+  a: new DistributeWebhookCommand(payload, target),
+  b: new DistributeWebhookCommand(payload, target2),
+  c: new DistributeWebhookCommand(payload2, target),
+  d: new DistributeWebhookCommand(payload2, target2),
+};
+
+const inputObservable = {
+  a: new WebhookSentEvent(payload, target, idempotentKey),
+  b: new WebhookSentEvent(payload, target2, idempotentKey),
+  c: new WebhookSentEvent(payload2, target, idempotentKey2),
+  d: new WebhookSentEvent(payload2, target2, idempotentKey2),
+};
+
 describe('WebhookSaga', () => {
   let scheduler: TestScheduler;
   const saga = new WebhookSagas();
@@ -23,12 +37,11 @@ describe('WebhookSaga', () => {
 
   describe('Debounce', () => {
     it('Should return a command', () => {
-      const events$ = of(new WebhookSentEvent(payload, target, idempotentKey));
-      scheduler.run(({ expectObservable }) => {
+      scheduler.run(({ expectObservable, cold }) => {
+        const inputMarbles = '(a|)';
         const expectedMarble = '(a|)';
-        const expectedObservable = {
-          a: new DistributeWebhookCommand(payload, target),
-        };
+
+        const events$ = cold(inputMarbles, inputObservable);
         expectObservable(saga.webhookSent(events$)).toBe(
           expectedMarble,
           expectedObservable,
@@ -37,15 +50,11 @@ describe('WebhookSaga', () => {
     });
 
     it('Should return only one command for 2 events chained', () => {
-      const events$ = of(
-        new WebhookSentEvent(payload, target, idempotentKey),
-        new WebhookSentEvent(payload, target, idempotentKey),
-      );
-      scheduler.run(({ expectObservable }) => {
+      scheduler.run(({ expectObservable, cold }) => {
+        const inputMarbles = '(aa|)';
         const expectedMarble = '(a|)';
-        const expectedObservable = {
-          a: new DistributeWebhookCommand(payload, target),
-        };
+
+        const events$ = cold(inputMarbles, inputObservable);
         expectObservable(saga.webhookSent(events$)).toBe(
           expectedMarble,
           expectedObservable,
@@ -55,14 +64,9 @@ describe('WebhookSaga', () => {
 
     it('Should return 2 commands for 2 events separated by more than a second', () => {
       scheduler.run(({ cold, expectObservable }) => {
-        const inputObservable = {
-          a: new WebhookSentEvent(payload, target, idempotentKey),
-        };
-        const inputMarbles = '   a 1002ms a';
-        const expectedMarble = '1000ms a 1002ms a';
-        const expectedObservable = {
-          a: new DistributeWebhookCommand(payload, target),
-        };
+        const inputMarbles = '  a      1002ms a';
+        const expectedMarble = '1000ms a      1002ms a';
+
         const events$ = cold(inputMarbles, inputObservable);
         expectObservable(saga.webhookSent(events$)).toBe(
           expectedMarble,
@@ -74,16 +78,11 @@ describe('WebhookSaga', () => {
 
   describe('Multiple targets', () => {
     it('Should return 2 commands for 2 events with different targets', () => {
-      const events$ = of(
-        new WebhookSentEvent(payload, target, idempotentKey),
-        new WebhookSentEvent(payload, target2, idempotentKey),
-      );
-      scheduler.run(({ expectObservable }) => {
+      scheduler.run(({ expectObservable, cold }) => {
+        const inputMarbles = '(ab|)';
         const expectedMarble = '(ab|)';
-        const expectedObservable = {
-          a: new DistributeWebhookCommand(payload, target),
-          b: new DistributeWebhookCommand(payload, target2),
-        };
+
+        const events$ = cold(inputMarbles, inputObservable);
         expectObservable(saga.webhookSent(events$)).toBe(
           expectedMarble,
           expectedObservable,
@@ -94,16 +93,11 @@ describe('WebhookSaga', () => {
 
   describe('Multiple idempotent key', () => {
     it('Should return 2 commands for 2 events with different idempotent key', () => {
-      const events$ = of(
-        new WebhookSentEvent(payload, target, idempotentKey),
-        new WebhookSentEvent(payload2, target, idempotentKey2),
-      );
-      scheduler.run(({ expectObservable }) => {
-        const expectedMarble = '(ab|)';
-        const expectedObservable = {
-          a: new DistributeWebhookCommand(payload, target),
-          b: new DistributeWebhookCommand(payload2, target),
-        };
+      scheduler.run(({ expectObservable, cold }) => {
+        const inputMarbles = '(ac|)';
+        const expectedMarble = '(ac|)';
+
+        const events$ = cold(inputMarbles, inputObservable);
         expectObservable(saga.webhookSent(events$)).toBe(
           expectedMarble,
           expectedObservable,
@@ -114,24 +108,11 @@ describe('WebhookSaga', () => {
 
   describe('Multiple idempotent key and targets', () => {
     it('Should return 4 commands for 4 events with different idempotent key and targets', () => {
-      const events$ = of(
-        new WebhookSentEvent(payload, target, idempotentKey),
-        new WebhookSentEvent(payload, target, idempotentKey),
-        new WebhookSentEvent(payload2, target, idempotentKey2),
-        new WebhookSentEvent(payload2, target, idempotentKey2),
-        new WebhookSentEvent(payload, target2, idempotentKey),
-        new WebhookSentEvent(payload, target2, idempotentKey),
-        new WebhookSentEvent(payload2, target2, idempotentKey2),
-        new WebhookSentEvent(payload2, target2, idempotentKey2),
-      );
-      scheduler.run(({ expectObservable }) => {
-        const expectedMarble = '(abcd|)';
-        const expectedObservable = {
-          a: new DistributeWebhookCommand(payload, target),
-          b: new DistributeWebhookCommand(payload2, target),
-          c: new DistributeWebhookCommand(payload, target2),
-          d: new DistributeWebhookCommand(payload2, target2),
-        };
+      scheduler.run(({ expectObservable, cold }) => {
+        const inputMarbles = '         aabbccddaaa';
+        const expectedMarble = '1000ms ---b-c-d--a';
+
+        const events$ = cold(inputMarbles, inputObservable);
         expectObservable(saga.webhookSent(events$)).toBe(
           expectedMarble,
           expectedObservable,
